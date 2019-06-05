314a1d407c93201db4389c3fd22734f23ae92b07
Prakhar Mishra <prakhmis@cisco.com>
2019-06-05 23:51:56 +0530

    suggested changes in sorting algo

--- src/main/java/training/Sorter.java
+++ src/main/java/training/Sorter.java
@@ -1,5 +1,7 @@
 package training;
 
+import java.util.Arrays;
+
 public class Sorter {
 
     public static <T extends Comparable<T>> void doBubbleSort(T[] arrInput) {
@@ -10,18 +12,19 @@ public class Sorter {
 
         if(nArrLength < 2)
             return;
-
-        boolean bSwapsInPass = false;
-        for(int nPassIdx= 0; nPassIdx < nArrLength; nPassIdx++){
-            bSwapsInPass = false;
-            for(int nElemIdx= 0; nElemIdx < nArrLength-nPassIdx-1; nElemIdx++){
+        int nEachPassLimit = nArrLength -1;
+        int nLastSwapIdx ;
+        while(true){
+            nLastSwapIdx = 0;
+            for(int nElemIdx= 0; nElemIdx < nEachPassLimit; nElemIdx++){
                 if((arrInput[nElemIdx].compareTo(arrInput[nElemIdx+1])) > 0){
                     swap(arrInput, nElemIdx, nElemIdx+1);
-                    bSwapsInPass = true;
+                    nLastSwapIdx = nElemIdx;
                 }
             }
-            if(!bSwapsInPass)
+            if(nLastSwapIdx == 0)
                 break;
+            nEachPassLimit = nLastSwapIdx;
         }
     }
 
@@ -52,18 +55,20 @@ public class Sorter {
             return;
 
         int nArrLength = arrInput.length;
+        int nElemIdx;
+        T nMinVal;
 
         if(nArrLength < 2)
             return;
 
         for (int nCurrIdx = 1; nCurrIdx < nArrLength; nCurrIdx++) {
-            for(int nElemIdx = nCurrIdx ; nElemIdx > 0 ; nElemIdx--){
-                if((arrInput[nElemIdx-1].compareTo(arrInput[nElemIdx])) > 0){
-                    swap(arrInput, nElemIdx, nElemIdx-1);
-                }else{
+            nMinVal = arrInput[nCurrIdx];
+            for(nElemIdx = nCurrIdx ; nElemIdx > 0 ; nElemIdx--){
+                if((arrInput[nElemIdx-1].compareTo(nMinVal)) <= 0)
                     break;
-                }
+                arrInput[nElemIdx] = arrInput[nElemIdx - 1];
             }
+            arrInput[nElemIdx] = nMinVal;
         }
     }
 
@@ -119,5 +124,56 @@ public class Sorter {
             arrInput[nElemIdx1] = arrInput[nElemIdx2];
             arrInput[nElemIdx2] = temp;
         }
+    }
+
+    public static void main(String[] args) {
+        Test1();
+        Test2();
+    }
+
+    private static void Test1() {
+        Integer[] items1 = {1, 5, 3, 2, 4};
+        System.out.println("\nBefore sort: " + Arrays.toString(items1));
+        Sorter.doInsertionSort(items1);
+        System.out.println("After Insertion sort: " + Arrays.toString(items1));
+
+        Integer[] items2 = {1, 5, 3, 2, 4};
+        System.out.println("\nBefore sort: " + Arrays.toString(items2));
+        Sorter.doBubbleSort(items2);
+        System.out.println("After Bubble sort: " + Arrays.toString(items2));
+
+        Integer[] items3 = {1, 5, 3, 2, 4};
+        System.out.println("\nBefore sort: " + Arrays.toString(items3));
+        Sorter.doSelectionSort(items3);
+        System.out.println("After Selection sort: " + Arrays.toString(items3));
+
+        Integer[] items4 = {1, 5, 3, 2, 4};
+        System.out.println("\nBefore sort: " + Arrays.toString(items4));
+        Sorter.doQuickSort(items4);
+        System.out.println("After Quick sort: " + Arrays.toString(items4));
+
+    }
+
+    private static void Test2() {
+        Integer[] items1 = {0, 2, 1, 3, 4};;
+        System.out.println("\nBefore sort: " + Arrays.toString(items1));
+        Sorter.doInsertionSort(items1);
+        System.out.println("After Insertion sort: " + Arrays.toString(items1));
+
+        Integer[] items2 = {0, 2, 1, 3, 4};;
+        System.out.println("\nBefore sort: " + Arrays.toString(items2));
+        Sorter.doBubbleSort(items2);
+        System.out.println("After Bubble sort: " + Arrays.toString(items2));
+
+        Integer[] items3 = {0, 2, 1, 3, 4};;
+        System.out.println("\nBefore sort: " + Arrays.toString(items3));
+        Sorter.doSelectionSort(items3);
+        System.out.println("After Selection sort: " + Arrays.toString(items3));
+
+        Integer[] items4 = {0, 2, 1, 3, 4};;
+        System.out.println("\nBefore sort: " + Arrays.toString(items4));
+        Sorter.doQuickSort(items4);
+        System.out.println("After Quick sort: " + Arrays.toString(items4));
+
     }
 }
